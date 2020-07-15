5ec67894abeb1e583b561ea099ba9db3b8f6a3f8
sor-ee <68268283+sor-ee@users.noreply.github.com>
2020-07-15 20:34:16 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+num1=input("enter float number1 : ")
+numm1=float(num1)
+num2=input("enter float number2 : ")
+numm2=float(num2)
+num3=input("enter float number3 : ")
+numm3=float(num3)
+print("\n")
+print("value nummber1 : "+num1)
+print("value nummber2 : "+num2)
+print("value nummber3 : "+num3)
+
+print("total all : "+str(numm1+numm2+numm3))
