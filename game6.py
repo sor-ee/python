c7c1322079f85bd55dcfd3a505009445a21374cf
sor-ee <68268283+sor-ee@users.noreply.github.com>
2020-10-09 14:58:23 +0700

    Add files via upload

--- game6.py
+++ game6.py
@@ -1,9 +1,12 @@
 import pgzrun
 def draw():
-    screen.fill('white')
-    screen.draw.text('use variable keyboard in update',(200,10),color='blue')
+    screen.fill(bgcolor)
+    screen.draw.text('use variable keyboard in update',(200,20),color='blue')
     apple.draw()
+    if live_bullet:
+        bullet.draw()
 def update():
+    global live_bullet
     if (keyboard.LEFT):
         apple.x -= 10
     elif (keyboard.RIGHT):
@@ -12,11 +15,30 @@ def update():
         apple.y -= 10
     elif (keyboard.DOWN):
         apple.y += 10
+    #check bullet and move
+    if (live_bullet):
+        bullet.y -=10
+        if bullet.y < 0:
+            live_bullet = False
 def on_key_down(key,mod,unicode):
-    pass
+    global bgcolor,live_bullet
+    if key == keys.R:
+        bgcolor = (255,0,0)
+    elif key == keys.B:
+        bgcolor = (0,0,0)
+    elif key == keys.G:
+        bgcolor = (0,255,0)
+    elif key == keys.W:
+        bgcolor = (255,255,255)
+    if key == keys.SPACE and live_bullet == False:
+        live_bullet = True
+        bullet.pos = apple.pos
 def on_key_up(key,mod):
     pass
 WIDTH = 640
 HEIGHT = 480
 apple = Actor('apple',(WIDTH/2,HEIGHT/2))
+bullet = Actor('pic',(-50,-50))
+live_bullet = False
+bgcolor = (255,255,255)
 pgzrun.go()
